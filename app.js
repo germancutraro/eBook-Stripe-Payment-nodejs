cd366ab4714bbc07ef14fa321a0cc0dc6a14c162
Germán <germancutraro@hotmail.com>
2017-10-13 20:35:30 -0300

    Fix port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // Middlewares
 app.use(bodyParser.json());
